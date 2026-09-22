4f1ac1044109d8a27487b914f12510a2f22d51f9
Marwan Karim <hakaishin@marwans-air.students.netlogon.liu.se>
2026-09-22 12:48:33 +0200

    written comment for Djikstra

--- R/dijkstra.R
+++ R/dijkstra.R
@@ -1,1 +1,15 @@
-#Had to start
+#' Dijkstra's Algorithm
+#'
+#' Computes the shortest path distances from a starting node
+#' to all other nodes in a weighted graph using Dijkstra's algorithm.
+#'
+#' @param graph A numeric matrix representing the weighted graph.
+#' @param source A numeric scalar indicating the starting node.
+#'
+#' @return A list containing the shortest distances and the previous
+#' nodes used to reconstruct the shortest paths.
+#'
+#' @references
+#' https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+#'
+#' @export
