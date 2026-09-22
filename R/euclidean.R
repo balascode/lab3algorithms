ee8612b5592d915667ad911050eccfce2b8f2c73
ANUPOJU VENKATA BALAJI <163442336+balascode@users.noreply.github.com>
2026-09-22 12:21:57 +0200

    Updated the description and added the datatype validation

--- R/euclidean.R
+++ R/euclidean.R
@@ -1,5 +1,23 @@
-gcd <- function(a, b) {
+#' Euclidean Algorithm
+#'
+#' Computes the greatest common divisor of two numbers
+#' using the Euclidean algorithm.
+#'
+#' @param a A numeric scalar.
+#' @param b A numeric scalar.
+#'
+#' @return The greatest common divisor of a and b.
+#'
+#' @references
+#' https://en.wikipedia.org/wiki/Euclidean_algorithm
+#'
+#' @export
 
+euclidean <- function(a, b) {
+  if((is.numeric(a) && length(a) == 1 &&
+      is.numeric(b) && length(b) == 1)){
+    stop()
+  }
   while (b != 0) {
     t <- b
     b <- a %% b
@@ -8,5 +26,3 @@ gcd <- function(a, b) {
 
   return(a)
 }
-
-gcd(48, 18)
