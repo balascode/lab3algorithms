4b8c9b0a3370f11ff723954bedbaf60973be884e
ANUPOJU VENKATA BALAJI <163442336+balascode@users.noreply.github.com>
2026-09-23 09:06:04 +0200

    Documented wiki_graph and completed package checks

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Example Graph for Dijkstra's Algorithm
+#'
+#' A weighted graph used to demonstrate Dijkstra's
+#' shortest path algorithm.
+#'
+#' @format A data frame with 18 rows and 3 variables:
+#' \describe{
+#'   \item{v1}{The starting node of an edge.}
+#'   \item{v2}{The destination node of an edge.}
+#'   \item{w}{The weight of the edge.}
+#' }
+#'
+#' @source
+#' https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+"wiki_graph"
